c2e57e3f4947967bd5a2a3b5e2dbd9aa660be350
foxching <rechielagria@gmail.com>
2023-09-16 17:18:52 +0800

    Styles, Fonts & Images

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import Link from "next/link";
 import React from "react";
+import Logo from "./dojo-logo.png";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav>
+      <Image
+        src={Logo}
+        alt="Dojo Helpdesk logo"
+        width={70}
+        quality={100}
+        placeholder="blur"
+      />
       <h1>Dojo Helpdesk</h1>
       <Link href="/">Dashboard</Link>
       <Link href="/tickets">Tickets</Link>
