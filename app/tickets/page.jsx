7ce90bbe28f9383e4574ccdbc63df59e10d5f0ca
foxching <rechielagria@gmail.com>
2023-09-16 20:41:33 +0800

    Dynamic Segments (Params)

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -4,8 +4,14 @@ import TicketList from "./TicketList";
 export default function Tickets() {
   return (
     <main>
-      <h1>Tickets</h1>
-      <p>Currently open tickets</p>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently open tickets.</small>
+          </p>
+        </div>
+      </nav>
       <TicketList />
     </main>
   );
